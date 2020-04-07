00fb7634069b7643d7d41ec91bb9f43a91f4243e
vidyaRane <62541890+vidyaRane@users.noreply.github.com>
2020-04-07 17:36:52 +0530

    Delete sample2.py

--- sample2.py
+++
@@ -1,4 +0,0 @@
-mydict = {'StuName': 'Steve', 'StuAge': 4, 'StuCity': 'Agra'}
-mydict['StuClass'] = 'Jr.KG'
-print("Student Name is:", mydict['StuName'])
-print("Student Class is:", mydict['StuClass'])
